9e67def2599c96ef4af89af984a43374dbf84899
vinay <abc@gmail.com>
2025-06-07 22:47:32 +0530

    sample js code

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('Hello from Kubernetes + Jenkins!'));
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
