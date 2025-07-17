9005cddca384a39424f98b576b778b576a7f02ec
vinay <abc@gmail.com>
2025-07-17 23:46:30 +0530

    c

--- shell-programs/disk_usage.sh
+++ shell-programs/disk_usage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-usage=`df -h . | tail -1 | awk -F " " '{print $(NF-1)}' | sed 's/%//g'`
+#usage=`df -h . | tail -1 | awk -F " " '{print $(NF-1)}' | sed 's/%//g'`
+usage=$(df -h . | tail -1 | awk -F " " '{print $(NF-1)}' | sed 's/%//g')
 
 if [ $usage -ge 80 ]; then
     echo "disk usage is above 80%"
