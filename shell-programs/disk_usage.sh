b8e43d427547c23facbf8dd41e2f73e1db44b45e
vinay <abc@gmail.com>
2025-07-17 23:38:44 +0530

    disk usage

---
+++ shell-programs/disk_usage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+usage= `df -h . | tail -1 | awk -F " " '{print $(NF-1)}' | sed 's/%//g'`
+
+if [ $usage -ge 80 ]; then
+    echo "disk usage is above 80%"
+    echo "current usage $usage %"
+else
+    echo "disk usage is ok"
+    echo "current usage $uage %"
+fi
